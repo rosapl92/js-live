859fc0bd0e8ba72beaf83a9d185a35a70acfdb3b
rosapl92 <palermorosa@hotmail.it>
2022-08-10 23:08:16 +0200

    javascript le basi

---
+++ assets/js/convertitore.js
@@ -0,0 +1,30 @@
+/*
+Lezione 27/28
+
+Scrivere un convertitore di unità di misura (g/Kg) utilizzando alcuni dei concetti
+visti nelle basi di JS
+ */
+
+function convertitore(value, unita, unitaDaCambiare) {
+    var result = {
+
+}
+
+
+    return retult;
+}
+
+var v1 = convertitore(10, g, Kg);
+console.log(v1);
+
+
+function convertitorex(valueOb, unitaDaCambiare1) {
+    return v = valueOb.value;
+    return u = unitaDaCambiare1.unita;
+
+    return {
+        value: 22;
+        unita: unitaDaCambiare1;
+    }
+}
+
